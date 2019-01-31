8263def158003b308acb34474d05be69ebabcee7
Thomas Higginbotham <thomas.higginbotham@experis.com>
2019-01-31 10:55:08 -0500

    Add library and integration app

---
+++ projects/dialog/src/lib/dialog.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'raven-dialog',
+  template: `
+    <p>
+      dialog works!
+    </p>
+  `,
+  styles: []
+})
+export class DialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
